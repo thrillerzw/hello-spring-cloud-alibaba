ddadb22d2329c4ccfee161bb7418add36d3cecc8
thrillerzw <295380007@qq.com>
2023-04-09 09:17:59 +0800

    网关添加sentinel限流

---
+++ hello-spring-cloud-alibaba-gateway/src/main/java/cn/mirrorming/spring/cloud/gateway/config/SentinelConfig.java
@@ -0,0 +1,21 @@
+package cn.mirrorming.spring.cloud.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class SentinelConfig {
+    public SentinelConfig(){
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                return ServerResponse.ok().body(Mono.just("限流，请求太频繁"),String.class);
+            }
+        });
+    }
+}
+
